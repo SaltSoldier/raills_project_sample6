cec13fee74d1e6e81a5f790f9b8eb337f3b2d929
SaltSoldier <soltsoldier0730rs@gmail.com>
2016-06-29 00:27:20 +0900

    add index of comment to articles/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do 
    resources :comments, only: [:create]
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
