86585eaafe973dfd20d9ab4a74f97e4ee2efe0a0
SaltSoldier <soltsoldier0730rs@gmail.com>
2016-07-13 13:24:05 +0900

    run 'rails g decorator'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = ArticleDecorator.decorate_collection(Article.all)
   end
 
   # GET /articles/1
